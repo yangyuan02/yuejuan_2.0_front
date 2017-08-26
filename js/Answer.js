a788558e8a4ef34f8f95f16421909d09455760de
yangyuan <1367511704@qq.com>
2017-08-26 16:56:12 +0800

    no massege

--- js/Answer.js
+++ js/Answer.js
@@ -50,7 +50,7 @@ m1.controller("demo", function ($scope, $timeout, $http) {
             }
         });
     }
-    // $scope.getAnswer()
+    $scope.getAnswer()
     $scope.oldAnswerLen = answer_id.length
     //点击显示
     $scope.add = function (index) {
@@ -169,7 +169,7 @@ m1.controller("demo", function ($scope, $timeout, $http) {
             obj.plaids = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
         }
         $scope.append(obj)
-        // $scope.createAsswer(obj)
+        $scope.createAsswer(obj)
         clear()
         close()
         console.log($scope.listObj)
