177253922d0272ae4b2d6e266dc8c2a94b25f47f
zhaozhengguang <zhaozhengguang@zhaozhengguang.local>
2017-12-06 10:32:09 +0800

    xx

--- js/edit_bj.js
+++ js/edit_bj.js
@@ -28,35 +28,61 @@ var isLogin = localStorage.getItem("token");
   var exam_subject_id = getUrlParam(url,'exam_subject_id');
   console.log(docx_id);
   console.log(docx_num);
-$.ajax({
-   	type: "GET",
-   	url: ajaxIp+"/api/v2/question_banks?docx_id="+docx_id+"",
-  	dataType: "JSON",
-  	data:{'limit':null,'page':null},
-  	headers: {'Authorization': "Bearer " + isLogin},
-  	success: function(data){
-  		console.log(data);
-  		 var pp='<p>'+data[docx_num].content+'</p>';
-  		 console.log(pp);
-  		 UE.getEditor('container').setContent(''+pp+'');
-  		 // $('#container').append(pp);
-  		// for (var i = 0; i < data.length; i++) {
-  			
-  			// $('#item-ul').append('<li class="items"><i class="item-op "><a href="javascript:;" class="save-btn right"><i class="iconfont">&#xe653;</i>保存</a><a href="javascript:;" class="look-detail right"><i class="iconfont">&#xe699;</i>查看解析</a></i><div class="item-cont editor-enabled" contenteditable="true"><p>'+data[i].content+'</p></div><ul class="bottom-btn"><li><a class="item-edit" data-num="'+i+'"><i class="iconfont">&#xe614;</i>题干编辑</a></li><li><a href="javascript:;" class="item-seg"><i class="iconfont">&#xe636;</i>分割试题</a></li><li><a href="javascript:;" class="item-insert"><i class="iconfont">&#xe601;</i>题组插入</a></li><li><a href="javascript:;" class="item-merge"><i class="iconfont">&#xe689;</i>向下合并</a></li><li><a href="javascript:;" class="grade-set"><i class="iconfont">&#xe630;</i>设定分指</a></li><li><a href="javascript:;" class="item-up"><i class="iconfont">&#xe631;</i>上移</a></li><li><a href="javascript:;" class="item-down"><i class="iconfont">&#xe607;</i>下移</a></li><li><a href="javascript:;" class="item-dele"><i class="iconfont">&#xe616;</i>删除</a></li><li><a href="javascript:;" class="determine bind-item">绑定题组</a></li></ul></li>');
-         
-         // 
-  		// };
-    },
-    error: function(){
-    	// alert('请稍后从新尝试登录或者联系管理员');
-    	// localStorage.clear();
-    	// window.location.href = './login'
-    }
-  });
+// $.ajax({
+//    	type: "GET",
+//    	url: ajaxIp+"/api/v2/question_banks?docx_id="+docx_id+"",
+//   	dataType: "JSON",
+//   	data:{'limit':null,'page':null},
+//   	headers: {'Authorization': "Bearer " + isLogin},
+//   	success: function(data){
+//   		console.log(data);
+//   		 var pp='<p>'+data[docx_num].content+'</p>';
+//   		 console.log(pp);
+//   		 UE.getEditor('container').setContent(''+pp+'');
+  		
+//     },
+//     error: function(){
+    
+//     }
+//   });
 $(".p_top a").click(function(event) {
 $(this).attr('href', 'edit_paper?docx_id='+docx_id+'&exam_subject_id='+exam_subject_id+'');
 
 });
+//获取题目内容，答案。。。
+ $.ajax({
+         type: "POST",
+         url: ajaxIp + "/api/v2/question_banks/update_question",
+         data: {
+             'id':id,
+            
+         },
+         headers: { 'Authorization': "Bearer " + isLogin },
+         success: function(data) {
+         console.log(data);
+         var a=data.content;
+         var b=data.answer;
+         var c=data.difficulty_level;
+         var d=data.analysis;
+         console.log(b);
+         UE.getEditor('container').setContent(''+a+'');
+         //答案
+         if(b!==undefined){
+           UE.getEditor('container02').setContent(''+b+'');
+         }
+         //解析
+          if(d!==undefined){
+           UE.getEditor('container03').setContent(''+d+'');
+         }
+         //难度
+         if(c!==undefined){
+           $(".dif_input input").val(c);
+         }
+         },
+         error: function() {
+
+         }
+     });
 //保存题目内容
  $(".edit_bj_btn01").click(function(event) {
   var a=UE.getEditor('container').getContent();
@@ -69,8 +95,8 @@ $(this).attr('href', 'edit_paper?docx_id='+docx_id+'&exam_subject_id='+exam_subj
          },
          headers: { 'Authorization': "Bearer " + isLogin },
          success: function(data) {
-
-
+         console.log(data);
+          window.location.reload();
 
          },
          error: function() {
@@ -80,6 +106,112 @@ $(this).attr('href', 'edit_paper?docx_id='+docx_id+'&exam_subject_id='+exam_subj
      alert(UE.getEditor('container').getContent());
 
  });
+ //答案输入
+$(".edit_li_btn01").click(function(event) {
+          /* Act on the event */
+          $(".edit_li_div01").slideDown(500);
+});
+
+$(".edit_li_div01_btn01").click(function(event) {
+ 
+ $(".edit_li_div01").slideUp(500);
+
+var a=UE.getEditor('container02').getContentTxt();
+
+     $.ajax({
+         type: "POST",
+         url: ajaxIp + "/api/v2/question_banks/update_question",
+         data: {
+             'id':id,
+             'answer': a,
+         },
+         headers: { 'Authorization': "Bearer " + isLogin },
+         success: function(data) {
+         console.log(data);
+          window.location.reload();
+
+         },
+         error: function() {
+
+         }
+     });
+
+
+ });
+$(".edit_li_div01_btn02").click(function(event) {
+ 
+ $(".edit_li_div01").slideUp(500);
+ });
+//答案解析
+$(".edit_li_btn02").click(function(event) {
+        
+$(".edit_li_div02").slideDown(500);
+});
+$(".edit_li_div02_btn01").click(function(event) { 
+
+var a=UE.getEditor('container03').getContent();
+
+     $.ajax({
+         type: "POST",
+         url: ajaxIp + "/api/v2/question_banks/update_question",
+         data: {
+             'id':id,
+             'analysis': a,
+         },
+         headers: { 'Authorization': "Bearer " + isLogin },
+         success: function(data) {
+         console.log(data);
+         window.location.reload();
+
+
+         },
+         error: function() {
+
+         }
+     });
+
+
+     $(".edit_li_div02").slideUp(500);
+});
+$(".edit_li_div02_btn02").click(function(event) {       
+     $(".edit_li_div02").slideUp(500);
+});
+// 设置难度
+// $(".dif_btn").click(function(event) {
+//   $(".dif_input").show();
+//   $(".dif_btn").hide();
+// });
+
+// $(".dif_input button").click(function(event) {
+//   var a=$(".dif_input input").val();
+
+//      $.ajax({
+//          type: "POST",
+//          url: ajaxIp + "/api/v2/question_banks/update_question",
+//          data: {
+//              'id':id,
+//              'difficulty_level': a,
+//          },
+//          headers: { 'Authorization': "Bearer " + isLogin },
+//          success: function(data) {
+//          console.log(data);
+//          window.location.reload();
+
+//          },
+//          error: function() {
+
+//          }
+//      });
+//       $(".dif_input").hide();
+//       $(".dif_btn").show();
+// });
+
+
+
+
+
+
+
 
 
 
